6344d17e202c367d5a103d081558e170146b55e3
jeman618 <jcisne23@calpoly.edu>
2025-11-03 11:32:02 -0800

    changes

--- packages/proto/src/<component>.ts
+++ packages/proto/src/<component>.ts
@@ -1,21 +1,19 @@
-// src/destination.ts
 import { html, css, LitElement } from "lit";
 import { property } from "lit/decorators.js";
 import reset from "./styles/reset.css.ts";
 
-export class DestinationElement extends LitElement {
+export class TestElement extends LitElement {
     @property({type : String}) name = "Wonderer";
   override render() {
     return html`
       <h2>Welcome, ${this.name}!</h2>
-      <p>This is your Lit domain.</p>
     `;
   }
 
   static styles = [
     reset.styles,
     css`
-        h2, p {
+        h2 {
             text-align: left;
         }
     `
